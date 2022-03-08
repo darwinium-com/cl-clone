8ea3dc0f6cd27c96dbe3c02f81a733c86fdfa8da
Chris Adams <chris@cadams.com.au>
2022-03-08 16:11:49 +1100

    Allow DB to be persisted from its own directory

--- craigslist_proj/settings.py
+++ craigslist_proj/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# When running in Docker, expect the DB in /db/ so we can persist outside the container
+DB_DIR = os.environ.get('DB_DIR') or BASE_DIR
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
@@ -81,7 +84,7 @@ WSGI_APPLICATION = 'craigslist_proj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(DB_DIR, 'db.sqlite3'),
     }
 }
 
